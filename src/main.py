2b203dd8b15b77327f498b6d97934271eff48034
Saiful Islam Dipak <saiful.kbtel@gmail.com>
2025-03-18 12:29:35 +0600

    Add CORS middleware to allow cross-origin requests

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.routers import users, auth
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:4173"],  # List of allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 app.include_router(users.router)
 app.include_router(auth.router)
